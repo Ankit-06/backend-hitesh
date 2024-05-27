0cbdc6e48a2c488aba56c6f28edf439ce3a96ad2
Ankit Mishra <ankitm@smartek21.com>
2024-05-28 00:28:33 +0530

    utils code discussion

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+//app.use is for adding middlewares
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" })); //accepting json data of 16kb limit. (body-parser is an alternate, which is not required anymore)
+app.use(express.urlencoded({ extended: true })); //with extended we can use objects inside objects as well
+app.use(express.static("public")); //configuration for adding publically available assets inside public folder
+app.use(cookieParser()); //config for cookies
+
+export { app };
